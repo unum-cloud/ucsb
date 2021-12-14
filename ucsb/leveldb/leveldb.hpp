3e23d5f4b33aa316d11955c4b11f2bef7d91b065
Mesrop Gevorgyan <79257290+mgevu@users.noreply.github.com>
2021-12-14 18:39:43 +0400

    LevelDB: No need to format key

--- ucsb/leveldb/leveldb.hpp
+++ ucsb/leveldb/leveldb.hpp
@@ -28,7 +28,7 @@ using operation_result_t = ucsb::operation_result_t;
 
 struct leveldb_t : public ucsb::db_t {
   public:
-    inline leveldb_t() : db_(nullptr), slice_buffer_(100) {}
+    inline leveldb_t() : db_(nullptr) {}
     ~leveldb_t() override = default;
 
     bool init(fs::path const& config_path, fs::path const& dir_path) override;
@@ -55,10 +55,8 @@ struct leveldb_t : public ucsb::db_t {
     };
 
     inline bool load_config(fs::path const& config_path, config_t& config);
-    inline leveldb::Slice build_slice(key_t key);
 
     leveldb::DB* db_;
-    std::vector<char> slice_buffer_;
 };
 
 bool leveldb_t::init(fs::path const& config_path, fs::path const& dir_path) {
@@ -96,7 +94,7 @@ void leveldb_t::destroy() {
 operation_result_t leveldb_t::insert(key_t key, value_spanc_t value) {
     std::string data(reinterpret_cast<char const*>(value.data()), value.size());
     leveldb::WriteOptions wopt;
-    leveldb::Slice slice = build_slice(key);
+    leveldb::Slice slice {reinterpret_cast<char*>(&key), sizeof(key)};
     leveldb::Status status = db_->Put(wopt, slice, data);
     if (!status.ok())
         return {0, operation_status_t::error_k};
@@ -106,7 +104,7 @@ operation_result_t leveldb_t::insert(key_t key, value_spanc_t value) {
 operation_result_t leveldb_t::update(key_t key, value_spanc_t value) {
 
     std::string data;
-    leveldb::Slice slice = build_slice(key);
+    leveldb::Slice slice {reinterpret_cast<char*>(&key), sizeof(key)};
     leveldb::Status status = db_->Get(leveldb::ReadOptions(), slice, &data);
     if (status.IsNotFound())
         return {1, operation_status_t::not_found_k};
@@ -123,7 +121,7 @@ operation_result_t leveldb_t::update(key_t key, value_spanc_t value) {
 
 operation_result_t leveldb_t::remove(key_t key) {
     leveldb::WriteOptions wopt;
-    leveldb::Slice slice = build_slice(key);
+    leveldb::Slice slice {reinterpret_cast<char*>(&key), sizeof(key)};
     leveldb::Status status = db_->Delete(wopt, slice);
     if (!status.ok())
         return {0, operation_status_t::error_k};
@@ -133,7 +131,7 @@ operation_result_t leveldb_t::remove(key_t key) {
 
 operation_result_t leveldb_t::read(key_t key, value_span_t value) const {
     std::string data;
-    leveldb::Slice slice = build_slice(key);
+    leveldb::Slice slice {reinterpret_cast<char*>(&key), sizeof(key)};
     leveldb::Status status = db_->Get(leveldb::ReadOptions(), slice, &data);
     if (status.IsNotFound())
         return {1, operation_status_t::not_found_k};
@@ -147,9 +145,9 @@ operation_result_t leveldb_t::read(key_t key, value_span_t value) const {
 operation_result_t leveldb_t::batch_read(keys_span_t keys) const {
 
     // Note: imitation of batch read!
-    for (auto const& key : keys) {
+    for (auto key : keys) {
         std::string data;
-        leveldb::Slice slice = build_slice(key);
+        leveldb::Slice slice {reinterpret_cast<char*>(&key), sizeof(key)};
         leveldb::Status status = db_->Get(leveldb::ReadOptions(), slice, &data);
     }
     return {keys.size(), operation_status_t::ok_k};
@@ -158,7 +156,7 @@ operation_result_t leveldb_t::batch_read(keys_span_t keys) const {
 operation_result_t leveldb_t::range_select(key_t key, size_t length, value_span_t single_value) const {
 
     leveldb::Iterator* db_iter = db_->NewIterator(leveldb::ReadOptions());
-    leveldb::Slice slice = build_slice(key);
+    leveldb::Slice slice {reinterpret_cast<char*>(&key), sizeof(key)};
     db_iter->Seek(slice);
     size_t selected_records_count = 0;
     for (int i = 0; db_iter->Valid() && i < length; i++) {
@@ -202,12 +200,6 @@ bool leveldb_t::load_config(fs::path const& config_path, config_t& config) {
     config.filter_bits = j_config.value("filter_bits", 10);
 
     return true;
-}
-
-leveldb::Slice leveldb_t::build_slice(key_t key) {
-    char str_format[] = "%d";
-    int len = std::snprintf(slice_buffer_.data(), slice_buffer_.size(), str_format, key);
-    return leveldb::Slice(slice_buffer_.data(), len);
 }
 
 } // namespace google
