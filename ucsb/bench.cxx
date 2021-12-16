42977b5ab92d20f8f2fc5c4117aee085977cdeda
Mesrop Gevorgyan <79257290+mgevu@users.noreply.github.com>
2021-12-16 15:17:56 +0400

    Add RAM usage statistics

--- ucsb/bench.cxx
+++ ucsb/bench.cxx
@@ -11,6 +11,7 @@
 #include "ucsb/core/transaction.hpp"
 #include "ucsb/core/factory.hpp"
 #include "ucsb/core/operation.hpp"
+#include "ucsb/core/stat.hpp"
 #include "ucsb/core/exception.hpp"
 #include "ucsb/core/format.hpp"
 
@@ -27,6 +28,7 @@ using operation_kind_t = ucsb::operation_kind_t;
 using operation_status_t = ucsb::operation_status_t;
 using operation_result_t = ucsb::operation_result_t;
 using operation_chooser_t = std::unique_ptr<ucsb::operation_chooser_t>;
+using mem_stat_t = ucsb::mem_stat_t;
 using exception_t = ucsb::exception_t;
 using printable_bytes_t = ucsb::printable_bytes_t;
 
@@ -162,6 +164,8 @@ void transaction(bm::State& state, workload_t const& workload, db_t& db) {
     size_t fails = 0;
     size_t operations_done = 0;
     size_t bytes_processed_cnt = 0;
+    mem_stat_t mem_stat(workload.records_count / 1000);
+    mem_stat.start();
 
     for (auto _ : state) {
         operation_result_t result;
@@ -177,15 +181,18 @@ void transaction(bm::State& state, workload_t const& workload, db_t& db) {
         default: throw exception_t("Unknown operation"); break;
         }
 
+        operations_done += result.depth;
         bool success = result.status == operation_status_t::ok_k;
         fails += size_t(!success) * result.depth;
-        operations_done += result.depth;
         bytes_processed_cnt += size_t(success) * workload.value_length * result.depth;
     }
 
+    mem_stat.stop();
+    state.SetBytesProcessed(bytes_processed_cnt);
     state.counters["fails,%"] = bm::Counter(fails * 100.0 / operations_done);
     state.counters["operations/s"] = bm::Counter(operations_done - fails, bm::Counter::kIsRate);
-    state.SetBytesProcessed(bytes_processed_cnt);
+    state.counters["mem_max"] = bm::Counter(mem_stat.rss().max, bm::Counter::kDefaults, bm::Counter::kIs1024);
+    state.counters["mem_avg"] = bm::Counter(mem_stat.rss().avg, bm::Counter::kDefaults, bm::Counter::kIs1024);
 }
 
 int main(int argc, char** argv) {
