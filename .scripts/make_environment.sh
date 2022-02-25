70a3ee77d5035a09b166b8f2fe09991e4ae83821
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2022-02-25 13:21:22 +0000

    Update: make_environment.sh

--- .scripts/make_environment.sh
+++ .scripts/make_environment.sh
@@ -4,7 +4,18 @@ unset HISTFILE
 mkdir -p .scripts
 remote_url=http://gitlab.unum.am/devops/buildsystem/-/raw/main
 
-wget -q -O ./.scripts/update_env.sh $remote_url/.scripts/update_env.sh
-# wget -q -O ./README.md $remote_url/README.md
+# check connection
+#https://stackoverflow.com/questions/929368/how-to-test-an-internet-connection-with-bash
+wget -q --spider http://gitlab.unum.am
 
-bash -i ./.scripts/update_env.sh
+if [ $? -ne 0 ]; then
+    echo "You have no connection with gitlab.unum.am: Check your connection..."
+    echo "You have no connection with gitlab.unum.am: Check your connection..." >> ./.scripts/.log
+    exit
+fi
+
+wget -q -O ./.scripts/prepare_env.sh $remote_url/.scripts/prepare_env.sh
+bash -i .scripts/prepare_env.sh
+
+# $1 is token, $2 is branch_name
+bash -i ./.scripts/make_env.sh "$1" "$2"
