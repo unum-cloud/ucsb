f5c41e9a419020e77dbf8f3a7e1be942d590a2d4
Mesrop Gevorgyan <79257290+mgevu@users.noreply.github.com>
2022-06-14 15:24:46 +0400

    UnumDB: fixed crash

--- ucsb/unumdb/unumdb.hpp
+++ ucsb/unumdb/unumdb.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <memory>
 #include <vector>
 #include <fmt/format.h>
 
@@ -30,6 +31,7 @@ using transaction_t = ucsb::transaction_t;
 
 using fingerprint_t = key_t;
 using region_t = region_gt<key_t, data_source_t::unfixed_size_k>;
+using region_ptr_t = std::unique_ptr<region_t>;
 using building_holder_t = building_holder_gt<fingerprint_t>;
 
 /**
@@ -37,7 +39,7 @@ using building_holder_t = building_holder_gt<fingerprint_t>;
  */
 struct unumdb_t : public ucsb::db_t {
   public:
-    inline unumdb_t() : region_("", "./", region_config_t(), {}), thread_idx_(0) {}
+    inline unumdb_t() : thread_idx_(0) {}
     inline ~unumdb_t() { close(); }
 
     void set_config(fs::path const& config_path, fs::path const& dir_path) override;
@@ -96,7 +98,7 @@ struct unumdb_t : public ucsb::db_t {
     db_config_t config_;
 
     resources_ptr_t resources_;
-    region_t region_;
+    region_ptr_t region_;
 
     // for every thread we use different darray to collect buildings
     darray_gt<darray_gt<building_holder_t>> import_data_;
@@ -139,11 +141,11 @@ bool unumdb_t::open() {
         return false;
 
     auto region_config = create_region_config(config_.user_config);
-    region_ = region_t("Kovkas", dir_path_.c_str(), region_config, resources_);
+    region_ = std::make_unique<region_t>("Kovkas", dir_path_.c_str(), region_config, resources_);
     import_data_.resize(config_.user_config.threads_max_cnt);
 
     // Cleanup directories
-    if (!region_.population_count()) {
+    if (!region_->population_count()) {
         for (auto const& path : config_.paths)
             ucsb::clear_directory(path.c_str());
     }
@@ -152,56 +154,57 @@ bool unumdb_t::open() {
 }
 
 bool unumdb_t::close() {
+    if (resources_ && region_ && !region_->name().empty())
+        region_->flush();
+
+    region_.reset();
+    config_.clear();
     if (resources_) {
-        if (!region_.name().empty())
-            region_.flush();
-        region_ = region_t("", "./", region_config_t(), {});
         resources_->disk_router.reset();
         resources_->fibers->stop_and_shutdown();
     }
-    config_.clear();
     resources_.reset();
 
     return true;
 }
 
 void unumdb_t::destroy() {
-    region_.destroy();
+    region_->destroy();
 }
 
 operation_result_t unumdb_t::upsert(key_t key, value_spanc_t value) {
     citizen_view_t citizen {reinterpret_cast<byte_t const*>(value.data()), value.size()};
-    region_.insert(key, citizen);
+    region_->insert(key, citizen);
     return {1, operation_status_t::ok_k};
 }
 
 operation_result_t unumdb_t::update(key_t key, value_spanc_t value) {
 
     citizen_location_t location;
-    region_.find(key, location);
+    region_->find(key, location);
     if (!location)
         return {1, operation_status_t::not_found_k};
 
     citizen_view_t citizen {reinterpret_cast<byte_t const*>(value.data()), value.size()};
-    region_.insert(key, citizen);
+    region_->insert(key, citizen);
     return {1, operation_status_t::ok_k};
 }
 
 operation_result_t unumdb_t::remove(key_t key) {
-    region_.remove(key);
+    region_->remove(key);
     return {1, operation_status_t::ok_k};
 }
 
 operation_result_t unumdb_t::read(key_t key, value_span_t value) const {
     citizen_location_t location;
-    region_.find(key, location);
+    region_->find(key, location);
     if (!location)
         return {1, operation_status_t::not_found_k};
 
     countdown_t countdown;
     notifier_t read_notifier(countdown);
     citizen_span_t citizen {reinterpret_cast<byte_t*>(value.data()), value.size()};
-    region_.select(location, citizen, read_notifier);
+    region_->select(location, citizen, read_notifier);
     if (!countdown.wait(*resources_->fibers))
         return {0, operation_status_t::error_k};
 
@@ -210,9 +213,9 @@ operation_result_t unumdb_t::read(key_t key, value_span_t value) const {
 
 operation_result_t unumdb_t::batch_upsert(keys_spanc_t keys, values_spanc_t values, value_lengths_spanc_t sizes) {
 
-    region_.insert({keys.data(), keys.size()},
-                   {reinterpret_cast<byte_t const*>(values.data()), values.size()},
-                   {sizes.data(), sizes.size()});
+    region_->insert({keys.data(), keys.size()},
+                    {reinterpret_cast<byte_t const*>(values.data()), values.size()},
+                    {sizes.data(), sizes.size()});
     return {keys.size(), operation_status_t::ok_k};
 }
 
@@ -230,12 +233,12 @@ operation_result_t unumdb_t::batch_read(keys_spanc_t keys, values_span_t values)
         size_t batch_size = std::min(config_.uring_queue_depth, fingerprints.size() - current_idx);
         size_t found_buffer_size = 0;
         darray_gt<citizen_location_t> locations(batch_size);
-        region_.find(fingerprints.view().subspan(current_idx, batch_size), locations.span(), found_buffer_size);
+        region_->find(fingerprints.view().subspan(current_idx, batch_size), locations.span(), found_buffer_size);
 
         if (found_buffer_size) {
             countdown_t countdown(batch_size);
             span_gt<byte_t> buffer_span(reinterpret_cast<byte_t*>(values.data()) + buffer_offset, found_buffer_size);
-            region_.select(locations.view(), buffer_span, countdown);
+            region_->select(locations.view(), buffer_span, countdown);
             if (!countdown.wait(*resources_->fibers))
                 return {0, operation_status_t::error_k};
             found_cnt += batch_size;
@@ -284,10 +287,10 @@ operation_result_t unumdb_t::range_select(key_t key, size_t length, values_span_
     citizen_size_t citizen_aligned_max_size =
         ucsb::roundup_to_multiple<ucsb::values_buffer_t::alignment_k>(config_.user_config.unfixed_citizen_max_size);
 
-    region_.lock_shared();
+    region_->lock_shared();
     size_t buffer_offset = 0;
-    auto it = region_.find(key);
-    for (size_t i = 0; it != region_.end() && i < length; ++it, ++i) {
+    auto it = region_->find(key);
+    for (size_t i = 0; it != region_->end() && i < length; ++it, ++i) {
         if (!it.is_removed()) {
             citizen_span_t citizen {reinterpret_cast<byte_t*>(values.data()) + buffer_offset, citizen_aligned_max_size};
             countdown.increment(1);
@@ -302,7 +305,7 @@ operation_result_t unumdb_t::range_select(key_t key, size_t length, values_span_
             task_cnt = 0;
         }
     }
-    region_.unlock_shared();
+    region_->unlock_shared();
 
     return {selected_records_count, operation_status_t::ok_k};
 }
@@ -313,9 +316,9 @@ operation_result_t unumdb_t::scan(key_t key, size_t length, value_span_t single_
     citizen_span_t citizen {reinterpret_cast<byte_t*>(single_value.data()), single_value.size()};
     size_t scanned_records_count = 0;
 
-    region_.lock_shared();
-    auto it = region_.find(key);
-    for (size_t i = 0; it != region_.end() && i < length; ++it, ++i) {
+    region_->lock_shared();
+    auto it = region_->find(key);
+    for (size_t i = 0; it != region_->end() && i < length; ++it, ++i) {
         if (!it.is_removed()) {
             countdown.reset(1);
             it.get(citizen, countdown);
@@ -323,7 +326,7 @@ operation_result_t unumdb_t::scan(key_t key, size_t length, value_span_t single_
             ++scanned_records_count;
         }
     }
-    region_.unlock_shared();
+    region_->unlock_shared();
     return {scanned_records_count, operation_status_t::ok_k};
 }
 
@@ -338,12 +341,12 @@ void unumdb_t::flush() {
             return left.schema.lower_fingerprint < right.schema.upper_fingerprint;
         });
 
-        region_.import(import_data.view());
+        region_->import(import_data.view());
         import_data.clear();
         import_data_.clear();
     }
     else {
-        region_.flush();
+        region_->flush();
     }
 }
 
@@ -360,7 +363,7 @@ size_t unumdb_t::size_on_disk() const {
 }
 
 std::unique_ptr<transaction_t> unumdb_t::create_transaction() {
-    return std::make_unique<unumdb_transaction_t>(region_.create_transaction(), config_.uring_queue_depth, resources_);
+    return std::make_unique<unumdb_transaction_t>(region_->create_transaction(), config_.uring_queue_depth, resources_);
 }
 
 bool unumdb_t::load_config() {
