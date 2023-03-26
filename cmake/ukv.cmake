c8d444f749924602fbce03023b729d979546fe20
Mesrop <79257290+mgevor@users.noreply.github.com>
2023-03-26 22:51:39 +0400

    Fix: Disable unnecessary warning

--- cmake/ukv.cmake
+++ cmake/ukv.cmake
@@ -50,6 +50,7 @@ ExternalProject_Add(
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DENABLE_STATIC:STRING=ON
     -DENABLE_CPPSUITE:BOOL=OFF
+    -DCMAKE_C_FLAGS=-Wno-maybe-uninitialized
     
     "${UKV_BUILD_ARGS}"
 )
